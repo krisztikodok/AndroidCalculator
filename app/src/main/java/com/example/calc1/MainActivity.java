e58da42bf57f1dd456728d7d40399e8a2bdf271c
krisztikodok <personalpkhu@gmail.com>
2023-07-01 05:04:10 +0200

    layout done,some methods done

--- app/src/main/java/com/example/calc1/MainActivity.java
+++ app/src/main/java/com/example/calc1/MainActivity.java
@@ -7,19 +7,22 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import java.io.IOError;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView result,solution;
+    TextView result_tv, solution_tv;
 
     //buttons
-    MaterialButton button_a,button_b,button_c, button_d,
-            button_e,button_f, button_g,button_h,
-            button_i,button_j,button_k,button_l,
-            button_m,button_n,button_o,button_p,
-            button_q,button_r,button_s,button_t;
-
-
+    MaterialButton button_a, button_b, button_c, button_d,
+            button_e, button_f, button_g, button_h,
+            button_i, button_j, button_k, button_l,
+            button_m, button_n, button_o, button_p,
+            button_q, button_r, button_s, button_t;
 
 
     @Override
@@ -28,44 +31,92 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         //assign textviews
-        result=findViewById(R.id.result);
-        solution=findViewById(R.id.solution);
+        result_tv = findViewById(R.id.result);
+        solution_tv = findViewById(R.id.solution);
         //assign buttons with IDs
-        assignID(button_a,R.id.button_a);
-        assignID(button_b,R.id.button_b);
-        assignID(button_c,R.id.button_c);
-        assignID(button_d,R.id.button_d);
-        //
-        assignID(button_e,R.id.button_e);
-        assignID(button_f,R.id.button_f);
-        assignID(button_g,R.id.button_g);
-        assignID(button_h,R.id.button_h);
-        //
-        assignID(button_i,R.id.button_i);
-        assignID(button_j,R.id.button_j);
-        assignID(button_k,R.id.button_k);
-        assignID(button_l,R.id.button_l);
-        //
-        assignID(button_m,R.id.button_m);
-        assignID(button_n,R.id.button_n);
-        assignID(button_o,R.id.button_o);
-        assignID(button_p,R.id.button_p);
-        //
-        assignID(button_q,R.id.button_q);
-        assignID(button_r,R.id.button_r);
-        assignID(button_s,R.id.button_s);
-        assignID(button_t,R.id.button_t);
+        //1st row
+        assignID(button_a, R.id.button_a);
+        assignID(button_b, R.id.button_b);
+        assignID(button_c, R.id.button_c);
+        assignID(button_d, R.id.button_d);
+        //2nd row
+        assignID(button_e, R.id.button_e);
+        assignID(button_f, R.id.button_f);
+        assignID(button_g, R.id.button_g);
+        assignID(button_h, R.id.button_h);
+        //3rd row
+        assignID(button_i, R.id.button_i);
+        assignID(button_j, R.id.button_j);
+        assignID(button_k, R.id.button_k);
+        assignID(button_l, R.id.button_l);
+        //4th row
+        assignID(button_m, R.id.button_m);
+        assignID(button_n, R.id.button_n);
+        assignID(button_o, R.id.button_o);
+        assignID(button_p, R.id.button_p);
+        //5th row
+        assignID(button_q, R.id.button_q);
+        assignID(button_r, R.id.button_r);
+        assignID(button_s, R.id.button_s);
+        assignID(button_t, R.id.button_t);
 
     }
 
     //method to assign buttons with IDs
-    void assignID(MaterialButton b, int id){
-        b=findViewById(id);
-        b.setOnClickListener(this);
+    void assignID(MaterialButton mat_b, int id) {
+        mat_b = findViewById(id);
+        mat_b.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        MaterialButton bt = (MaterialButton) v;
 
+        //getting the text
+        String bt_text = bt.getText().toString();
+        String toCalc=solution_tv.getText().toString();
+
+        // AC clears textviews
+        if(bt_text.equals("AC")){
+            solution_tv.setText("");
+            result_tv.setText("");
+            return;
+        }
+
+        // =
+        if(bt_text.equals("=")){
+            //calculate result
+            solution_tv.setText((result_tv.getText()));
+            return;
+        }
+
+        // clear last button input
+        else if(bt_text.equals("C")){
+            toCalc=toCalc.substring(0,toCalc.length()-1); //trim the last character
+            return;
+        }
+        else{ toCalc=toCalc+bt_text;} //concatenate input
+        solution_tv.setText(toCalc);
+        String finalSum=sum(toCalc);
+
+        if(!finalSum.equals("Error")){
+            if(bt_text.equals("=")){
+                result_tv.setText(finalSum);
+            }
+        }
+    }
+
+    // calculate result
+    String sum(String input){
+        try {
+            Context xc=Context.enter();
+            xc.setOptimizationLevel(-1);
+            Scriptable sc = xc.initStandardObjects();
+            String finalSum=xc.evaluateString(sc,input,"Javascript",1,null).toString();//calculate the string
+            return finalSum;
+        }catch (Exception e){
+            //result_tv.setText("Error");
+            return  "Error";
+        }
     }
 }
